8b3f5a0c0b3a3a98d976446626ac77b69bd896d1
henrryvalle12121212-sys <henrryvalle0919@gmail.com>
2026-06-19 14:31:21 -0500

    new1

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+self.addEventListener("install", event => {
+  console.log("Service Worker installed");
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.open("v1").then(cache => {
+      return cache.match(event.request).then(response => {
+        return response || fetch(event.request).then(networkResponse => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
+});
